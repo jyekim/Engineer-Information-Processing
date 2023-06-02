13af36dd73bc01b83d53f640b671224e5f42eb04
JIHYE <jye7921@gmail.com>
2023-06-02 14:25:36 +0900

    first

---
+++ src/main/java/com/example/demo/CalcController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalcController {
+	
+	@GetMapping(value="/")
+	public String index() {
+		return "Hello Git!";
+	}
+	
+	@GetMapping(value="/add")
+	public String add() {
+		return "25 + 36 = " + (25+36);
+	}
+	
+	@GetMapping(value="/sub")
+	public String sub() {
+		return "25 - 36 = " + (25-36);
+	}
+
+
+}
