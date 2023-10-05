6ca4aa0bf5735e639c6e1d587fedb4db6df37843
JIHYE <jye7921@gmail.com>
2023-10-05 19:32:39 +0900

    Add files via upload

---
+++ python문제/Q_02.py
@@ -0,0 +1,5 @@
+a = "REMEMBER NOVEMBER"
+b = a[:3] + a[12:16];
+c = "R AND %s" % "STR";
+
+print(b+c);
---
+++ python문제/Q_04.py
@@ -0,0 +1,7 @@
+a = {'한국', '중국', '일본'}
+a.add('베트남')
+a.add('중국')
+a.remove('일본')
+a.update({'홍콩', '한국', '태국'})
+print(a)
+print(type(a))
